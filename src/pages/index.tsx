224bb8c904e3426208fc6456b6691669e7c5b8b1
Bribri2525 <146964668+Bribri2525@users.noreply.github.com>
2024-05-13 11:19:34 +0800

    brianna adding pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import HomeScreen from "./HomeScreen";
+import ScheduleScreen from './ScheduleScreen';
+import SplashScreen from './SplashScreen';
+import TodoScreen from './TodoScreen';
+
+
+export  { HomeScreen, ScheduleScreen, SplashScreen, TodoScreen }
