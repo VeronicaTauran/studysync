c3c7dcb734c72ce90fb6c1791da77b85a94a4375
VeronicaTauran <113008526+VeronicaTauran@users.noreply.github.com>
2024-05-13 10:03:15 +0800

    fixing errors

--- src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import HomeScreen from "./HomeScreen";
-import ScheduleScreen from './ScheduleScreen';
-import SplashScreen from './SplashScreen';
-import TodoScreen from './TodoScreen';
-
-
-export  { HomeScreen, ScheduleScreen, SplashScreen, TodoScreen }
